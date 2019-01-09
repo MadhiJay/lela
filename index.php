d568f2824d1ce5bdb259336d4b4ec17fbb447256
madhi <upalakshi@gmail.com>
2019-01-10 00:00:29 +0530

    Home page navigation and signup form

--- index.php
+++ index.php
@@ -1,80 +1,325 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Project Lela</title>
 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+/* Style for slideshow */
+* {box-sizing: border-box;}
+body {font-family: Verdana, sans-serif;}
+.mySlides {display: none;}
+img {vertical-align: middle;}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width:2000px;
+  position: relative;
+  margin: auto;
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 30s;
+  animation-name: fade;
+  animation-duration: 30s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: 9} 
+  to {opacity: 10}
+}
+
+@keyframes fade {
+  from {opacity: 9} 
+  to {opacity: 10}
+}
+
+@media only screen and (max-width: 300px) {
+  .text {font-size: 11px}
+}
+
+/*Style for signup*/
+
+body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box;}
+
+/* Full-width input fields */
+input[type=text], input[type=password] {
+  width:50%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+/* Add a background color when the inputs get focus */
+input[type=text]:focus, input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+/* Set a style for all buttons */
+button {
+  background-color: #4CAF50;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 50%;
+  opacity: 0.9;
+}
+
+button:hover {
+  opacity:1;
+}
+
+/* Extra styles for the cancel button */
+.cancelbtn {
+  padding: 14px 20px;
+  background-color:red;
+}
+
+/* Float cancel and signup buttons and add an equal width */
+.cancelbtn,{
+  float: left;
+  width: 50%;
+}
+
+/* Add padding to container elements */
+.container {
+  padding:10px;
+}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 2; /* Sit on top */
+  left: 100px;
+  top: 50px;
+  width: 500px /* Full width */
+  height: 500px /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color:white;
+  opacity:4;
+  padding-top:0px;
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color:white;
+  margin: 5% auto 15% auto; /* 5% from the top, 15% from the bottom and centered */
+  border: 1px solid #888;
+  width: 100%; /* Could be more or less, depending on screen size */
+}
+
+/* Style the horizontal ruler */
+hr {
+  border: 1px solid #f1f1f1;
+  margin-bottom: 25px;
+}
+ 
+/* The Close Button (x) */
+.close {
+  position: absolute;
+  right: 35px;
+  top: 15px;
+  font-size: 40px;
+  font-weight: bold;
+  color:black;
+}
+
+.close:hover,
+.close:focus {
+  color: #f44336;
+  cursor: pointer;
+}
+
+/* Clear floats */
+.clearfix::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+/* Change styles for cancel button and signup button on extra small screens */
+@media screen and (max-width: 300px) {
+  .cancelbtn, .signupbtn {
+     width: 100%;
+  }
+}
+
+</style>
 </head>
 <body>
-<nav class="navbar navbar-expand-sm bg-primary navbar-dark">
-  <!-- Home --> 
-  <a class="navbar-brand" href="#">Home</a>
-
-  <ul class="navbar-nav">
-    <!-- Dropdown -->
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-        Accomandation
-      </a>
-      <div class="dropdown-menu">
-        <a class="dropdown-item" href="#">Deluxe Room</a>
-        <a class="dropdown-item" href="#">Bawa Room</a>
-        <a class="dropdown-item" href="#">Family Deluxe Room</a>
-        <a class="dropdown-item" href="#">Deluxe Suite</a>
-        <a class="dropdown-item" href="#">Bawa Suite</a>
-      </div>
-    </li>
-    <!-- Links -->
-    <li class="nav-item">
-      <a class="nav-link" href="#">Facilities & Services</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Locations</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Contact us</a>
-    </li>
-  </ul>
-  <button class="btn btn-danger navbar-btn">Book now</button>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">LELA</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Home</a></li>
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Become a host<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+        <li class="dropdown-header">HOMES</li>
+        <li><a href="#"> List Your Place </a></li>
+        
+        <li class="divider"></li>
+        <li class="dropdown-header">Rooms</li>
+        <li><a href="#">List Your Rooms</a></li>
+    </ul>
+      </li>
+      <li><a href="#">Locations around Ella</a></li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#" onclick="document.getElementById('id01').style.display='block'" style="width:auto;"><span class="glyphicon glyphicon-user" ></span>Sign Up</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    </ul>
+  </div>
 </nav>
 
+<div class="slideshow-container">
 
-<div class="container text-left" style= "display:flex">
-  <div class="page-header">
-  <img src="images/logolela.png" width=200px height=200px margin=0px>     
-  </div> 
-  <div>
-  <p class="font-weight-light" 
-  style="font-size:50px;color:Blue;padding:100px 100px 100px 0px;">HOTEL LELA</p>
-  </div> 
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="images/img_nature_wide.jpg" style="width:100%">
+  <div class="text"></div>
 </div>
 
-<div class="rows" style= "display:flex">
-  <div class="col-6"  style="background-color:lightblue">
-  <h3 style="color:white;padding:10px">Select dates to see our prices:</h3> 
-  <form>
-    <div class="md-form">
-    <label for="Checkin">Check-In</label>
-    <input placeholder="Selected date" type="text" id="indate" class="form-control datepicker">
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="images/img_snow_wide.jpg" style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="images/img_mountains_wide.jpg" style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
+
+<div id="id01" class="modal">
+  <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+  <form class="modal-content" action="/action_page.php">
+    <div class="container">
+      <h1>Sign Up</h1>
+      <p>Please fill in this form to create an account.</p>
+      <hr>
+      <label for="email"><b>Email</b></label><br>
+      <input type="text" placeholder="Enter Email" name="email" required>
+
+      <label for="psw"><b>Password</b></label><br>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+
+      <label for="psw-repeat"><b>Repeat Password</b></label><br>
+      <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+      
+      <label>
+        <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+      </label>
+
+      <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+
+      <div class="clearfix">
+        <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+        <button type="submit" class="signupbtn">Sign Up</button>
+      </div>
     </div>
   </form>
-  </div> 
-  <div>
-  
-  </div> 
 </div>
 
-<script type="text/javascript">
-    $(".form_datetime").datetimepicker({format: 'yyyy-mm-dd hh:ii'});
-</script>  
+<script>
+var slideIndex = 0;
+showSlides();
 
-  
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 30000); // Change image every 30 seconds
+}
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+
+
+
+
 
 </body>
 </html>
