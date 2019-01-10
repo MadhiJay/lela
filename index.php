3850709a948cb800b22c27602402cb91ae9ca31d
madhi <upalakshi@gmail.com>
2019-01-11 00:58:59 +0530

    login Modal and summary of thelocation around Ella

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  
+
+  <link href="/docs/4.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
   <link rel="stylesheet" href="styles/home-page.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -37,13 +41,12 @@
     </ul>
     <ul class="nav navbar-nav navbar-right">
       <li><a href="#" onclick="document.getElementById('id01').style.display='block'" style="width:auto;"><span class="glyphicon glyphicon-user" ></span>Sign Up</a></li>
-      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      <li><a href="#" onclick="document.getElementById('id02').style.display='block'" style="width:auto;"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
     </ul>
   </div>
 </nav>
 
 <div class="slideshow-container">
-
 <div class="mySlides fade">
   <div class="numbertext">1 / 4</div>
   <img src="images/hike.jpg" style="width:100%">
@@ -108,6 +111,84 @@
   </form>
 </div>
 
+<div id="id02" class="modal">
+  
+  <form class="modal-content animate" action="/action_page.php">
+    <div class="imgcontainer">
+      <span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">&times;</span>
+      <img src="images/img_avatar.png" alt="Avatar" class="avatar">
+      
+    </div>
+<br>
+    <div class="container">
+      <label for="uname"><b>Username</b></label><br>
+      <input type="text" placeholder="Enter Username" name="uname" required><br>
+
+      <label for="psw"><b>Password</b></label><br>
+      <input type="password" placeholder="Enter Password" name="psw" required><br>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label><br>
+
+      <button type="submit" class="signupbtn">Login</button><br>
+      
+      <button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Cancel</button>
+      <br>
+      <span class="psw">Forgot <a href="#">password?</a></span>
+
+    </div>
+
+  </form>
+</div>
+
+<div class="container" style="background-color:darkgray">
+<h1 style="align:center;">Top destinations for Ella trips</h1>
+<h2 style="align:center;">Pick your point of interest and find a hotel nearby</h2>
+<div class="accordian">
+    <ul>
+		<li>
+			<div class="image_title">
+				<a href="#">Nine arch bridge</a>
+			</div> 
+			<a href="#">
+				<img src="images/Trips/9arch.jpg"/>
+			</a>
+		</li>
+		<li>
+			<div class="image_title">
+				<a href="#">Bomburu Ella</a>
+			</div>
+			<a href="#">
+				<img src="images/Trips/bomburuella.jpg"/>
+			</a>
+		</li>
+		<li>
+			<div class="image_title">
+				<a href="#">Ella Rock</a>
+			</div>
+			<a href="#">
+				<img src="images/Trips/ellarock.jpg"/>
+			</a>
+		</li>
+		<li>
+			<div class="image_title">
+				<a href="#">Diyamula Falls</a>
+			</div>
+			<a href="#">
+				<img src="images/Trips/diyaluma.jpg"/>
+			</a>
+		</li>
+		<li>
+			<div class="image_title">
+				<a href="#">Lipton's Seat</a>
+			</div>
+			<a href="#">
+				<img src="images/Trips/liptonsseat.jpg"/>
+			</a>
+		</li>
+	</ul>
+</div>
+</div>
 <script>
 var slideIndex = 0;
 showSlides();
@@ -127,8 +208,9 @@ function showSlides()
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 30 seconds
+  setTimeout(showSlides, 10000); // Change image every 10 seconds
 }
+
 function signupModal(){
   // Get the modal
 var modal = document.getElementById('id01');
@@ -138,6 +220,19 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+}
+
+function loginModal()
+{
+  // Get the modal
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 }
 
