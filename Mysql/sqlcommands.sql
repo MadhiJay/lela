61ff5d5d873d84b712e2e847be22280f8a970773
2014ICT26 <lakabeyrathnna@gmail.com>
2019-01-20 11:02:42 +0530

    homerent table created

--- Mysql/sqlcommands.sql
+++ Mysql/sqlcommands.sql
@@ -14,3 +14,17 @@ CREATE PROCEDURE NewUser(IN username VARCHAR(100),IN email VARCHAR(100),IN pword
  INSERT INTO user (username,email,pword) values(username,email,pword);
  END //
 DELIMITER ;
+
+create table homeRent(
+	email varchar(100) primary key,
+	category varchar(100),
+	place varchar(100),
+	noOfRooms int,
+	price varchar(5),
+	descrpt varchar(500),
+	contact varchar(100),
+	img varchar(200),
+	foreign key(email) references user(email)
+
+
+);
